67f58b2672df0497868b6b42f44472597a128be1
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-02-16 17:38:56 -0500

    the units need to be destroyed as well

--- zookeeper-units/destroy.sh
+++ zookeeper-units/destroy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 fleetctl stop zookeeper.presence\@{1,2,3}.service
+fleetctl destroy zookeeper.presence\@{1,2,3}.service
 fleetctl stop zookeeper\@{1,2,3}.service
+fleetctl destroy zookeeper\@{1,2,3}.service
 fleetctl destroy zookeeper.presence\@.service
 fleetctl destroy zookeeper\@.service

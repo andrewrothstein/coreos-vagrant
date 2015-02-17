379096afe103add97bd2c00ed635d52f639eb56f
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-02-16 22:43:05 -0500

    cleaned up unit files. still some random problem with the ZK network connectivity. added a kafka service

--- zookeeper-units/test.sh
+++ zookeeper-units/test.sh
@@ -13,7 +13,7 @@ etcdctl ls /skydns/local/${ENV}/${CLUSTER}/ --recursive
 etcdctl get /skydns/local/${ENV}/${CLUSTER}/zookeeper-1
 
 echo "checking DNS resolution for zookeeper-%i from a separate container on host zookeeper@.%i..."
-fleetctl ssh zookeeper@1.service docker run -ti --dns=172.17.8.101 andrewrothstein/docker-dnsutils dig zookeeper-1
-fleetctl ssh zookeeper@2.service docker run -ti --dns=172.17.8.102 andrewrothstein/docker-dnsutils dig zookeeper-2
-fleetctl ssh zookeeper@3.service docker run -ti --dns=172.17.8.103 andrewrothstein/docker-dnsutils dig zookeeper-3
+fleetctl ssh zookeeper@1.service docker run -ti --dns=172.17.8.101 --dns-search=${CLUSTER}.${ENV}.local andrewrothstein/docker-dnsutils dig zookeeper-1.${CLUSTER}.${ENV}.local
+fleetctl ssh zookeeper@2.service docker run -ti --dns=172.17.8.102 --dns-search=${CLUSTER}.${ENV}.local andrewrothstein/docker-dnsutils dig zookeeper-2.${CLUSTER}.${ENV}.local
+fleetctl ssh zookeeper@3.service docker run -ti --dns=172.17.8.103 --dns-search=${CLUSTER}.${ENV}.local andrewrothstein/docker-dnsutils dig zookeeper-3.${CLUSTER}.${ENV}.local
 

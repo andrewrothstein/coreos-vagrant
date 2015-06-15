353444bd1df24487f08e4f007d90b42b10b57eac
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-06-14 22:18:05 -0400

    first boot of a mesos cluster

--- mesos-units/boot.sh
+++ mesos-units/boot.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-fleetctl submit mesos-master\@.service
-fleetctl start mesos-master\@1.service
-#fleetctl submit mesos-master\@.service mesos-slave.service
-#fleetctl start mesos-master\@{1,2,3}.service mesos-slave.service
+fleetctl submit mesos-master\@.service mesos-slave.service
+fleetctl start mesos-master\@{1,2,3}.service mesos-slave.service
--- mesos-units/destroy.sh
+++ mesos-units/destroy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-fleetctl stop mesos-master\@1.service
-fleetctl destroy mesos-master\@1.service
+fleetctl stop mesos-master\@{1,2,3}.service
+fleetctl stop mesos-slave.service
 
-#fleetctl stop mesos-master\@{1,2,3}.service
-#fleetctl stop mesos-slave.service
-#fleetctl destroy mesos-master\@{1,2,3}.service
-#fleetctl destroy mesos-slave.service
+fleetctl destroy mesos-master\@{1,2,3}.service
 fleetctl destroy mesos-master\@.service
+fleetctl destroy mesos-slave.service
